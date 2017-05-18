2f9c6b711975d2192d593990611d91b53ccf0ea1
TatriX <tatrics@gmail.com>
2017-05-19 00:22:55 +0300

    Updated babel

--- gulpfile.js
+++ gulpfile.js
@@ -24,6 +24,7 @@ gulp.task("js", function() {
         .pipe(sourcemaps.init())
         .pipe(babel({
             presets: ["es2015"],
+            plugins: ["transform-async-to-generator"],
         }))
         .pipe(concat("bundle.js"))
         .pipe(uglify())
